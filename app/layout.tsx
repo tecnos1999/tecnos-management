59eb7bcd124a930e9271709d93d3b47882705990
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-03 19:04:09 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,6 @@ export default function RootLayout({
           </SidebarContainer>
         </div>
       </ClientProvider>
-
       </body>
     </html>
   );
--- components/SidebarContainer.tsx
+++ components/SidebarContainer.tsx
@@ -1,20 +1,26 @@
-"use client"; 
+"use client";
 
 import React, { useState } from 'react';
 import Sidebar from "./Sidebar"; 
+import { usePathname } from "next/navigation";
 
 interface SidebarContainerProps {
   children: React.ReactNode;
 }
 
 const SidebarContainer: React.FC<SidebarContainerProps> = ({ children }) => {
-  const [isNavOpen, setIsNavOpen] = useState(true); 
+  const [isNavOpen, setIsNavOpen] = useState(true);
+  const pathname = usePathname(); 
+
+  const hideSidebar = pathname === "/login";
 
   return (
     <div className="flex h-screen w-full">
-      <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      {!hideSidebar && (
+        <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      )}
       
-      <div className={`flex-1 px-8  transition-all  duration-200 ${isNavOpen ? 'ml-64' : 'ml-16'}`}>
+      <div className={`flex-1 px-8 transition-all duration-200 ${!hideSidebar && isNavOpen ? 'ml-64' : 'ml-16'}`}>
         {children}
       </div>
     </div>
