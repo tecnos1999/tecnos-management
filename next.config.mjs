917c151ee19b001a6afc2ae42bc5b0a049d9477c
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-07 00:49:27 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         domains: ['gravatar.com'],
     },
-    assetPrefix: process.env.REACT_APP_ENV === 'development' 
-        ? 'http://tecnos-gateway:8080/admin-static' 
-        : 'http://tecnos-gateway:8080/admin-static',
+    assetPrefix: process.env.REACT_APP_ENV === 'development'
+        ? 'http://tecnos-gateway/ui-static'
+        : 'http://tecnos-gateway/ui-static',
 };
 
 export default nextConfig;
