7c1273764b4c0696e10b589f036e6a5163daf7fb
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-07 23:11:00 +0200

    update

--- module/system/service/ApiServer.tsx
+++ module/system/service/ApiServer.tsx
@@ -1,21 +1,21 @@
 import HttpResponse from "../httpResponse/HttpResponse";
 
-class ApiServer{
+class ApiServer {
+  api<U, T>(path: string, method: string, body: U, token: string): Promise<HttpResponse<T>> {
+    const port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+    const url = `http://${window.location.hostname}:${port}/server${path}`;
+    const isFormData = body instanceof FormData;
+    const options: RequestInit = {
+      method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        ...(isFormData ? {} : { "Content-Type": "application/json;charset=utf-8" }),
+      },
+      body: isFormData ? body : (body == null ? null : JSON.stringify(body)),
+    };
 
-    api<U, T>(path: string, method: string, body: U, token: string): Promise<HttpResponse<T>> {
-     const url = `http://${window.location.hostname}:8080/server` + path;
-     const isFormData = body instanceof FormData;
-     const options: RequestInit = {
-       method,
-       headers: {
-         Authorization: `Bearer ${token}`,
-         ...(isFormData ? {} : { "Content-Type": "application/json;charset=utf-8" }),
-       },
-       body: isFormData ? body : (body == null ? null : JSON.stringify(body)),
-     };
-   
-     return fetch(url, options);
-   }
-   }
-   
+    return fetch(url, options);
+  }
+}
+
 export default ApiServer;
