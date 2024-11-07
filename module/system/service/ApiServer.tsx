1962cafdd1de918780cf7298ac6f1155856c3d34
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-08 01:57:48 +0200

    LAST

--- module/system/service/ApiServer.tsx
+++ module/system/service/ApiServer.tsx
@@ -2,7 +2,7 @@ import HttpResponse from "../httpResponse/HttpResponse";
 
 class ApiServer {
   api<U, T>(path: string, method: string, body: U, token: string): Promise<HttpResponse<T>> {
-    const port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+    const port = process.env.REACT_APP_ENV  === 'production' ? 80 : 8080;
     const url = `http://${window.location.hostname}:${port}/server${path}`;
     const isFormData = body instanceof FormData;
     const options: RequestInit = {
