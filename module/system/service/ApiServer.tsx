c22791be4dd6fbd355d8b464d982396bdd905c6c
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-11 00:21:57 +0200

    update

--- module/system/service/ApiServer.tsx
+++ module/system/service/ApiServer.tsx
@@ -2,7 +2,7 @@ import HttpResponse from "../httpResponse/HttpResponse";
 
 class ApiServer {
   api<U, T>(path: string, method: string, body: U, token: string): Promise<HttpResponse<T>> {
-    const url = `http://${window.location.hostname}:8080/server${path}`;
+    const url = `https://${window.location.hostname}:8080/server${path}`;
     const isFormData = body instanceof FormData;
     const options: RequestInit = {
       method,
--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         domains: ['gravatar.com'],
     },
-    assetPrefix: process.env.REACT_APP_ENV === 'development' ?
-        'http://localhost:8080/admin-static' :
-        'http://89.33.44.227:8080/admin-static',
+    assetPrefix: process.env.REACT_APP_ENV === 'development'
+        ? 'http://localhost:8080/admin-static'
+        : 'https://89.33.44.227/admin-static',
 };
 
 export default nextConfig;
