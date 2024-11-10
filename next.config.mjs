e60e1cecae2e852f4cba7f226dcd4a8e81dd3fe4
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-10 03:21:34 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     },
     assetPrefix: process.env.REACT_APP_ENV === 'development' ?
         'http://localhost:8080/admin-static' :
-        'http://84.46.241.251:8080/admin-static',
+        'http://89.33.44.227:8080/admin-static',
 };
 
 export default nextConfig;
